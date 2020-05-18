2d112c9e52edbfc47d37b39c542d1a85dcce725e
Tomas <daugeltomas@gmail.com>
2020-05-18 09:53:34 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+"use strict";
+
+class Tv {
+    constructor(name, resolution, ratio,color,remote,type){
+        this.name = name;
+        this.resolution = resolution;
+        this.ratio = ratio;
+        this.color = color;
+        this.hasRemote = remote;
+        this.type = type;
+        this.turnedOn = false;
+        this.channel = '';
+        this.volume = 20;
+    }
+    
+    turnOn() {
+        if (this.turnedOn){
+            console.log(this.name + " jau yra ijungtas");
+            return;     
+        }
+        this.turnedOn = true;
+        console.log(this.name + " buvo ijungtas");
+    }
+    turnOff(){
+        if (!this.turnedOn){
+            console.log(this.name + " jau yra Isjungtas");
+            return;   
+        }
+        this.turnedOn = false;
+        console.log(this.name + " buvo isjungtas");
+    }
+    changeVolume(levelChange){
+        this.volume = this.volume + levelChange;
+        if (this.volume > 100){
+            this.volume = 100;
+        }
+        if (this.volume < 0){
+            this.volume = 0;
+        }
+        console.log(this.name +' garsas pasikeite i '+ this.volume+"."); 
+    }
+    currentVolume(){
+        console.log(this.name +' Dabartinis garsas yra '+ this.volume+"."); 
+    }
+    mute(){
+        this.volume = 0;
+        console.log(this.name +' Garsas Uzmutintas '); 
+    }
+}
+
+
+const silelis = new Tv("Silelis",[4000,3000],[4, 3], 'black', false, 'CRT');
+console.log(silelis);
+
+const lg = new Tv("lg",[8000,6000],[4,3],'silver',true,'flat');
+console.log(lg);
+
+silelis.turnOn();
+silelis.turnOff();
+
+lg.turnOn();
+
+lg.changeVolume(-20);
+lg.currentVolume();
+lg.mute();
+
+
